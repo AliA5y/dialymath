b67f7bb23665d58531e61974a4ce7ad665f91f18
AliA5y <ali.route9@gmail.com>
2024-10-29 11:13:46 +0300

    fix(group_card): remove card content over flow

--- lib/widgets/group_card.dart
+++ lib/widgets/group_card.dart
@@ -15,188 +15,217 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class GroupCard extends StatelessWidget {
-  const GroupCard({super.key,required this.group, required this.colorborder,required this.day,});
+  const GroupCard({
+    super.key,
+    required this.group,
+    required this.colorborder,
+    required this.day,
+  });
   final GroupModel group;
   final Color colorborder;
   final int day;
 
-
   @override
   Widget build(BuildContext context) {
     return Slidable(
-        startActionPane: ActionPane(
-                          motion: const StretchMotion(),
-                           children:[
-                             SlidableAction(
-                            backgroundColor:const Color.fromARGB(255, 65, 33, 243),
-                            label: 'edit',
-                            icon: Icons.edit,
-                            onPressed: (context)=>
-                            showModalBottomSheet(
-                            isScrollControlled: true,
-                            context: context,
-                            builder: (context) {
-                            return Editgroupsheet(day:day,group: group,);})),
-                             SlidableAction(
-                            backgroundColor:const Color.fromARGB(255, 223, 28, 28),
-                            label: 'delet',
-                            icon: Icons.delete,
-                            onPressed:(context)=>{
-                            group.delete(),
-                            //  BlocProvider.of<GroupCubit>(context).fetchAllgroups(day)
-                            }
-                            )
-                           ]
-                           ),
+      startActionPane: ActionPane(motion: const StretchMotion(), children: [
+        SlidableAction(
+            backgroundColor: const Color.fromARGB(255, 65, 33, 243),
+            label: 'edit',
+            icon: Icons.edit,
+            onPressed: (context) => showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return Editgroupsheet(
+                    day: day,
+                    group: group,
+                  );
+                })),
+        SlidableAction(
+            backgroundColor: const Color.fromARGB(255, 223, 28, 28),
+            label: 'delet',
+            icon: Icons.delete,
+            onPressed: (context) => {
+                  group.delete(),
+                  //  BlocProvider.of<GroupCubit>(context).fetchAllgroups(day)
+                })
+      ]),
       child: Container(
-        height: 150,
+        //TODO: remove the fixed hieght of the card
+        //TODO: to avoid contents overflow
         width: double.infinity,
-          padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16),
-          decoration: BoxDecoration(
-           border: BorderDirectional(
-           bottom:  BorderSide(width: 2,color: colorborder),
-            end: BorderSide(width: 0.5,color: colorborder) ),
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          border: BorderDirectional(
+              bottom: BorderSide(width: 2, color: colorborder),
+              end: BorderSide(width: 0.5, color: colorborder)),
           borderRadius: BorderRadius.circular(16),
-          ),
-          child: GestureDetector(
-            onTap: (){
-                  showModalBottomSheet(
-                  isScrollControlled:true,
-                  context: context,
-                  builder: (context) {
+        ),
+        child: GestureDetector(
+          onTap: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
                   return Test(dayname: "", day: day);
-                  });
-            },
-            child: Column(
-              children: [
-                ListTile(
-                  title:CoustmText(
-                    text: group.groupname??'',
+                });
+          },
+          child: Column(
+            children: [
+              ListTile(
+                  title: CoustmText(
+                    text: group.groupname ?? '',
                     textcolor: Colors.black,
                     textsize: 25,
                   ),
                   subtitle: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      CoustmText(text: 'grade ${group.groupgrade}',textcolor: Colors.black,textsize:16,),
-                      CoustmText(text: 'start at ${group.grouptime}',textcolor: Colors.black,textsize:16,),
+                      CoustmText(
+                        text: 'grade ${group.groupgrade}',
+                        textcolor: Colors.black,
+                        textsize: 16,
+                      ),
+                      CoustmText(
+                        text: 'start at ${group.grouptime}',
+                        textcolor: Colors.black,
+                        textsize: 16,
+                      ),
                     ],
                   ),
-                  trailing:Padding(
+                  trailing: Padding(
                     padding: const EdgeInsets.only(bottom: 6),
                     child: CoustmBticon(
-                    icon: Icons.group,height: 40,width: 50,
-                    ontab:(){
-                                   showModalBottomSheet(
-                                    isScrollControlled: true,
-                                    context: context,
-                                    builder: (context) {
-                                    return StudentSheet(day:day,);
-                                    });
-                    
-                      },
-                    ),
-                  )
-                    
-                )
-              ],
-            ),
-          ),
-        ),
-    );
-  }
-}
- class Studentcard extends StatelessWidget {
-  const Studentcard({super.key,required this.colorbordertow, required this.group,required this.day});
-    final Color colorbordertow;
-    final StudentModel group;
-    final int day;
-    
-  @override
-  Widget build(BuildContext context) {
-    final Uri whatsapp=Uri.parse('https://wa.me/+02${group.parentrhone}');
-    return Slidable(
-         startActionPane: ActionPane(
-                          motion: const StretchMotion(),
-                           children:[
-                             SlidableAction(
-                            backgroundColor:const Color.fromARGB(255, 65, 33, 243),
-                            label: 'edit',
-                            icon: Icons.edit,
-                            onPressed: (context)=>
-                            showModalBottomSheet(
+                      icon: Icons.group,
+                      height: 40,
+                      width: 50,
+                      ontab: () {
+                        showModalBottomSheet(
                             isScrollControlled: true,
                             context: context,
                             builder: (context) {
-                            return Editsheet(day:day,student:group,);})),
-                             SlidableAction(
-                            backgroundColor:const Color.fromARGB(255, 223, 28, 28),
-                            label: 'delet',
-                            icon: Icons.delete,
-                            onPressed:(context)=>{
-                            group.delete(),
-                            //  BlocProvider.of<GroupCubit>(context).fetchAllgroups(day)
-                            }
-                        
-                             
-                            )
-                           ]
-                           ),
-      child: Container(
-          padding: const EdgeInsets.only(top: 24,bottom: 24,left: 16),
-          decoration: BoxDecoration(
-           border: BorderDirectional(
-           bottom:  BorderSide(width: 2,color: colorbordertow),
-            end: BorderSide(width: 0.5,color: colorbordertow) ),
-          borderRadius: BorderRadius.circular(16),
+                              return StudentSheet(
+                                day: day,
+                              );
+                            });
+                      },
+                    ),
+                  ))
+            ],
           ),
-          child: GestureDetector(
-            onTap: (){
-               showModalBottomSheet(
-                              isScrollControlled: true,
-                              context: context,
-                              builder: (context) {
-                              return Editsheet(day:day,student:group,);});
-              
-            },
-            child: Column(
-              children: [
-                ListTile(
-                  title:CoustmText(
-                    text: group.studentname??"",
+        ),
+      ),
+    );
+  }
+}
+
+class Studentcard extends StatelessWidget {
+  const Studentcard(
+      {super.key,
+      required this.colorbordertow,
+      required this.group,
+      required this.day});
+  final Color colorbordertow;
+  final StudentModel group;
+  final int day;
+
+  @override
+  Widget build(BuildContext context) {
+    final Uri whatsapp = Uri.parse('https://wa.me/+02${group.parentrhone}');
+    return Slidable(
+      startActionPane: ActionPane(motion: const StretchMotion(), children: [
+        SlidableAction(
+            backgroundColor: const Color.fromARGB(255, 65, 33, 243),
+            label: 'edit',
+            icon: Icons.edit,
+            onPressed: (context) => showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return Editsheet(
+                    day: day,
+                    student: group,
+                  );
+                })),
+        SlidableAction(
+            backgroundColor: const Color.fromARGB(255, 223, 28, 28),
+            label: 'delet',
+            icon: Icons.delete,
+            onPressed: (context) => {
+                  group.delete(),
+                  //  BlocProvider.of<GroupCubit>(context).fetchAllgroups(day)
+                })
+      ]),
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          border: BorderDirectional(
+              bottom: BorderSide(width: 2, color: colorbordertow),
+              end: BorderSide(width: 0.5, color: colorbordertow)),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: GestureDetector(
+          onTap: () {
+            showModalBottomSheet(
+                isScrollControlled: true,
+                context: context,
+                builder: (context) {
+                  return Editsheet(
+                    day: day,
+                    student: group,
+                  );
+                });
+          },
+          child: Column(
+            children: [
+              ListTile(
+                  title: CoustmText(
+                    text: group.studentname ?? "",
                     textcolor: Colors.black,
                     textsize: 25,
                   ),
                   subtitle: Column(
-                   crossAxisAlignment: CrossAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Row(
                         children: [
-                          IconButton(onPressed: () async {
-                            final Uri uri = Uri(
-                            scheme: 'tel',
-                            path: group.parentrhone,
-                          );
-                          await launchUrl(uri);
-                          }, icon: const Icon(Icons.call),color:const Color.fromARGB(255, 17, 34, 184) ,),
-                          Text(group.parentrhone??''),
+                          IconButton(
+                            onPressed: () async {
+                              final Uri uri = Uri(
+                                scheme: 'tel',
+                                path: group.parentrhone,
+                              );
+                              await launchUrl(uri);
+                            },
+                            icon: const Icon(Icons.call),
+                            color: const Color.fromARGB(255, 17, 34, 184),
+                          ),
+                          Text(group.parentrhone ?? ''),
                         ],
                       ),
-                      IconButton(onPressed:(){launchUrl(whatsapp);},icon:const FaIcon(FontAwesomeIcons.whatsapp),color:const Color.fromARGB(255, 21, 124, 44),)
+                      IconButton(
+                        onPressed: () {
+                          launchUrl(whatsapp);
+                        },
+                        icon: const FaIcon(FontAwesomeIcons.whatsapp),
+                        color: const Color.fromARGB(255, 21, 124, 44),
+                      )
                     ],
                   ),
                   trailing: CoustmBticon(
-                    icon: Icons.delete,height: 40,width: 50,
-                    ontab:(){
+                    icon: Icons.delete,
+                    height: 40,
+                    width: 50,
+                    ontab: () {
                       group.delete();
                       // BlocProvider.of<GroupCubit>(context).fetchAllgroups(day);
-                      },
-                    )
-                )
-              ],
-            ),
+                    },
+                  ))
+            ],
           ),
         ),
+      ),
     );
   }
 }
