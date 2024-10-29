a1638f08e2d599d5eca48674e1c3c5835623806a
AliA5y <ali.route9@gmail.com>
2024-10-29 11:28:01 +0300

    removed the unneccesary bloc provider

--- lib/widgets/edit_groupsheet.dart
+++ lib/widgets/edit_groupsheet.dart
@@ -6,47 +6,39 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../screen/groups_screen/cubit/group_cubit.dart';
 
 class Editgroupsheet extends StatefulWidget {
-  const Editgroupsheet({super.key,required this.day, required this.group});
+  const Editgroupsheet({super.key, required this.day, required this.group});
   final int day;
   final GroupModel group;
   @override
   State<Editgroupsheet> createState() => _EditgroupsheetState();
 }
+
 class _EditgroupsheetState extends State<Editgroupsheet> {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context)=>GroupCubitCubit(),
-      child: BlocConsumer<GroupCubitCubit, GroupCubitState>(
-        listener: (context, state) {
-          if (state is AddgroupFailure){
+    // TODO: Here we removed the unneccesary bloc provider
+    return BlocConsumer<GroupCubitCubit, GroupCubitState>(
+      listener: (context, state) {
+        if (state is AddgroupFailure) {
           print('faield${state.errMessage}');
-          }
-          if (state is AddgroupSuccess){
-            // BlocProvider.of<GroupCubit>(context).fetchAllgroups(widget.day);
-            Navigator.pop(context);
-          }
-        },
-        builder: (context, state) {
-          return AbsorbPointer
-          (absorbing: state is AddgroupLoading ?true:false ,
-            child:Padding(
-              padding:EdgeInsets.only(
-                left: 16,
-                right: 16,
-                bottom: MediaQuery.of(context).viewInsets.bottom
-              ),
-              child: SingleChildScrollView
-              (child: Editgroupform(day: widget.day, group: widget.group)),
-            )
-            );
-        },
-      ),
+        }
+        if (state is AddgroupSuccess) {
+          // BlocProvider.of<GroupCubit>(context).fetchAllgroups(widget.day);
+          Navigator.pop(context);
+        }
+      },
+      builder: (context, state) {
+        return AbsorbPointer(
+            absorbing: state is AddgroupLoading ? true : false,
+            child: Padding(
+              padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
+              child: SingleChildScrollView(
+                  child: Editgroupform(day: widget.day, group: widget.group)),
+            ));
+      },
     );
   }
 }
-
-
-
-
-
